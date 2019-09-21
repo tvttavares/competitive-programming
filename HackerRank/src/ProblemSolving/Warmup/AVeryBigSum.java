87a21f813ae42d9b0fa77f7f67981372402585eb
Thiago <tvttavares@gmail.com>
2019-09-21 08:21:55 -0400

    A Very Big Sum

---
+++ HackerRank/src/ProblemSolving/Warmup/AVeryBigSum.java
@@ -0,0 +1,18 @@
+package ProblemSolving.Warmup;
+
+import java.util.Scanner;
+
+public class AVeryBigSum {
+	public static void main(String args[]) {
+
+		Scanner in = new Scanner(System.in);
+
+		int n = in.nextInt();
+		long sum = 0;
+
+		for (int i = 0; i < n; i++) {
+			sum += Long.valueOf(in.nextInt());
+		}
+		System.out.println(sum);
+	}
+}
