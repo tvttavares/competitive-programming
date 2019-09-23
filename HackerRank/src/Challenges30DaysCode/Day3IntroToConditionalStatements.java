948130aa4a53f2867fce7d00c19ea716bb659ad4
thiago <tvttavares@gmail.com>
2019-09-23 18:33:36 -0400

    Day 3: Intro to Conditional Statements

---
+++ HackerRank/src/Challenges30DaysCode/Day3IntroToConditionalStatements.java
@@ -0,0 +1,24 @@
+package Challenges30DaysCode;
+
+import java.util.Scanner;
+
+public class Day3IntroToConditionalStatements {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        
+        if (n % 2 == 1) {
+            System.out.println("Weird");
+        } else if (n % 2 == 0) {
+            if (n >= 2 && n <= 5)
+                System.out.println("Not Weird");
+            if (n >= 6 && n <= 20)
+                System.out.println("Weird");
+            if (n > 20)
+                System.out.println("Not Weird");
+        }
+    }
+}
