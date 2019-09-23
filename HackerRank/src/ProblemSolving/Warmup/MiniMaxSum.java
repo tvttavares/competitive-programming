d481ea6965e7ed1973c4a5ca859cf02f0ef3e467
thiago <tvttavares@gmail.com>
2019-09-23 18:47:46 -0400

    Mini-Max Sum

---
+++ HackerRank/src/ProblemSolving/Warmup/MiniMaxSum.java
@@ -0,0 +1,34 @@
+package ProblemSolving.Warmup;
+
+import java.util.Scanner;
+
+public class MiniMaxSum {
+
+	public static void main(String[] args) {
+
+		int qtd = 5;
+		long[] numeros = new long[5];
+		long total = 0, tot = 0, max = 0, min = 0;
+		Scanner scan = new Scanner(System.in);
+
+		for (int i = 0; i < qtd; i++) {
+			numeros[i] = scan.nextLong();
+			total += numeros[i];
+		}
+		scan.close();
+
+		min = total;
+		for (int i = 0; i < qtd; i++) {
+			tot = total;
+			tot -= numeros[i];
+
+			if (tot > max)
+				max = tot;
+			if (tot < min)
+				min = tot;
+
+		}
+		System.out.println(min + " " + max);
+	}
+
+}
