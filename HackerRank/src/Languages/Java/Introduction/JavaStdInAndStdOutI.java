9cb9a3e7a5f2dd832eb1682309548ede0d8f6f3d
thiago <tvttavares@gmail.com>
2019-09-25 20:37:57 -0400

    Java Stdin and Stdout I

---
+++ HackerRank/src/Languages/Java/Introduction/JavaStdInAndStdOutI.java
@@ -0,0 +1,18 @@
+package Languages.Java.Introduction;
+
+import java.util.Scanner;
+
+public class JavaStdInAndStdOutI {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		scan.close();
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
