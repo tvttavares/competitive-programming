ab385b5da97422c8942bad290578f791dc7d5255
thiago <tvttavares@gmail.com>
2020-01-28 12:14:11 -0400

    2D Array - DS

---
+++ HackerRank/src/DataStructures/Arrays/Array2DS.java
@@ -0,0 +1,36 @@
+package DataStructures.Arrays;
+
+import java.util.Scanner;
+
+public class Array2DS {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int[][] m = new int[6][6];
+		int cont = 0;
+		int max = Integer.MIN_VALUE;
+
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m.length; j++) {
+				m[i][j] = scan.nextInt();
+			}
+		}
+		scan.close();
+
+		for (int i = 0; i < m.length - 2; i++) {
+			for (int j = 0; j < m.length - 2; j++) {
+				cont = 0;
+				cont += m[i][j] + m[i][j + 1] + m[i][j + 2];
+				cont += m[i + 1][j + 1];
+				cont += m[i + 2][j] + m[i + 2][j + 1] + m[i + 2][j + 2];
+
+				if (cont > max)
+					max = cont;
+			}
+		}
+
+		System.out.println(max);
+
+	}
+
+}
