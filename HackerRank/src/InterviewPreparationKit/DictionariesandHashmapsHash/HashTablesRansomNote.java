17ab32c5116fc45d9c8947a38eda501687379474
thiago <tvttavares@gmail.com>
2020-01-29 23:46:24 -0300

    Hash Tables: Ransom Note

---
+++ HackerRank/src/InterviewPreparationKit/DictionariesandHashmapsHash/HashTablesRansomNote.java
@@ -0,0 +1,43 @@
+package InterviewPreparationKit.DictionariesandHashmapsHash;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class HashTablesRansomNote {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int m = scan.nextInt();
+		int n = scan.nextInt();
+		scan.nextLine();
+		Map<String, Integer> mag = new HashMap<>();
+
+		String[] magWords = scan.nextLine().split(" ");
+		String[] noteWords = scan.nextLine().split(" ");
+		scan.close();
+
+		for (String s : magWords) {
+			if (mag.containsKey(s)) {
+				mag.put(s, mag.get(s) + 1);
+			} else {
+				mag.put(s, 1);
+			}
+		}
+
+		for (String s : noteWords) {
+			if (!mag.containsKey(s)) {
+				System.out.print("No");
+				return;
+			}
+			int counter = mag.get(s) - 1;
+
+			if (counter == 0) {
+				mag.remove(s);
+			} else {
+				mag.put(s, counter);
+			}
+		}
+		System.out.print("Yes");
+	}
+}
