c7a5aa1b0a553b0e7d0f5a27a2bd2515d4187850
thiago <tvttavares@gmail.com>
2020-01-30 22:14:32 -0300

    Jumping on the Clouds

---
+++ HackerRank/src/InterviewPreparationKit/WarmupChallenges/JumpingOntheClouds.java
@@ -0,0 +1,36 @@
+package InterviewPreparationKit.WarmupChallenges;
+
+import java.util.Scanner;
+
+public class JumpingOntheClouds {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int nClouds = scan.nextInt();
+		int[] clouds = new int[nClouds];
+
+		for (int i = 0; i < nClouds; i++) {
+			clouds[i] = scan.nextInt();
+		}
+		scan.close();
+
+		System.out.println(jumpingClouds(clouds));
+	}
+
+	private static int jumpingClouds(int[] clouds) {
+		int current = clouds[0];
+		int maxJumps = 0;
+
+		while (current != clouds.length - 1) {
+			if (current + 2 < clouds.length && clouds[current + 2] == 0) {
+				current += 2;
+				maxJumps++;
+			} else {
+				current++;
+				maxJumps++;
+			}
+		}
+		return maxJumps++;
+	}
+
+}
