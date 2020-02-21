1bfba222d38247ca0919efba01bae7fb3084bf07
thiago <tvttavares@gmail.com>
2020-02-21 14:18:06 -0400

    Cats and a Mouse

---
+++ HackerRank/src/Algorithms/Implementation/CatsAndAMouse.java
@@ -0,0 +1,35 @@
+package Algorithms.Implementation;
+
+import java.util.Scanner;
+
+public class CatsAndAMouse {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int q = scan.nextInt();
+
+		while (q-- > 0) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			;
+			int c = scan.nextInt();
+			catAndMouse(a, b, c);
+		}
+		scan.close();
+
+	}
+
+	private static void catAndMouse(int a, int b, int c) {
+
+		if(Math.abs(c - a) == Math.abs(c - b)) {
+			System.out.println("Mouse C");
+		} else if (Math.abs(c - a) > Math.abs(c - b)) {
+			System.out.println("Cat A");
+		} else {
+			System.out.println("Cat B");
+		}
+		
+
+	}
+
+}
