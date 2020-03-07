a424cac96c1c84aa6a26f187e4cf965a1bf05b44
Thiago <tvttavares@gmail.com>
2020-03-07 15:01:53 -0400

    Day 0: Weighted Mean

---
+++ HackerRank/src/Challenges10DaysOfStatistics/Day0WeightedMean.java
@@ -0,0 +1,36 @@
+package Challenges10DaysOfStatistics;
+
+import java.util.Scanner;
+
+public class Day0WeightedMean {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int n = scan.nextInt();
+		double[] x = new double[n];
+		double[] w = new double[n];
+		for (int i = 0; i < n; i++) {
+			x[i] = scan.nextInt();
+		}
+		for (int i = 0; i < n; i++) {
+			w[i] = scan.nextInt();
+		}
+		scan.close();
+
+		weightedMean(x, w);
+
+	}
+
+	private static void weightedMean(double[] x, double[] w) {
+		double med = 0.0;
+		double sumW = 0.0;
+		for (int i = 0; i < x.length; i++) {
+			med += x[i] * w[i];
+			sumW += w[i];
+		}
+
+		System.out.printf("%.1f", med /= sumW);
+	}
+
+}
