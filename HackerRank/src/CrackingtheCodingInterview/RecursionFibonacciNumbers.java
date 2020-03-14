3db1b3abcc528013bdba2d2056ef360c481d3c08
Thiago <tvttavares@gmail.com>
2020-03-14 11:07:58 -0400

    Recursion: Fibonacci Numbers

---
+++ HackerRank/src/CrackingtheCodingInterview/RecursionFibonacciNumbers.java
@@ -0,0 +1,24 @@
+package CrackingtheCodingInterview;
+
+import java.util.Scanner;
+
+public class RecursionFibonacciNumbers {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int d = scan.nextInt();
+		scan.close();
+
+		System.out.println(fibo(d));
+
+	}
+
+	static long fibo(int n) {
+		if (n < 2) {
+			return n;
+		} else {
+			return fibo(n - 1) + fibo(n - 2);
+		}
+	}
+
+}
