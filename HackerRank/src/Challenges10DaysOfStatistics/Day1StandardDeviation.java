094f1a18c5a69bb2d212bb902a25cc21813024a5
Thiago <tvttavares@gmail.com>
2020-03-17 20:57:09 -0400

    Day 1: Standard Deviation

---
+++ HackerRank/src/Challenges10DaysOfStatistics/Day1StandardDeviation.java
@@ -0,0 +1,36 @@
+package Challenges10DaysOfStatistics;
+
+import java.util.Scanner;
+
+public class Day1StandardDeviation {
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		int q = scan.nextInt();
+
+		int[] arr = new int[q];
+		for (int i = 0; i < q; i++) {
+			arr[i] = scan.nextInt();
+		}
+		scan.close();
+
+		standartDeviation(arr);
+
+	}
+
+	private static void standartDeviation(int[] arr) {
+		double mean = 0;
+		double squardeDistance = 0;
+		for (int a : arr) {
+			mean += a;
+		}
+		mean /= arr.length;
+
+		for (int a : arr) {
+			squardeDistance += Math.pow((a - mean), 2);
+		}
+		squardeDistance = Math.sqrt((squardeDistance / arr.length));
+		System.out.printf("%.1f", squardeDistance);
+	}
+
+}
