69c9b2e408aee54d0b8d787a0801209230304e37
Thiago Tavares <tvttavares@gmail.com>
2020-07-18 22:16:24 -0400

    The Central Limit Theorem

---
+++ HackerRank/src/Challenges10DaysOfStatistics/Day6/TheCentralLimitTheoremIII.java
@@ -0,0 +1,19 @@
+package Challenges10DaysOfStatistics.Day6;
+
+public class TheCentralLimitTheoremIII {
+
+	public static void main(String[] args) {
+		/* Provided variables */
+		double mean = 500;
+		double std = 80;
+		int n = 100;
+		double zScore = 1.96; // equivalent to 95% confidence interval
+
+		/* Formula (found online) for confidence interval +/- */
+		double marginOfError = zScore * std / Math.sqrt(n);
+
+		/* Print output */
+		System.out.format("%.2f%n", mean - marginOfError);
+		System.out.format("%.2f%n", mean + marginOfError);
+	}
+}
